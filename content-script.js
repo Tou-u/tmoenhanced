c2420fceead40594daac6fcae326aa8613480c32
Tou-u <rodrignacio99@hotmail.com>
2022-08-23 02:57:27 -0400

    Un kilo de weas nuevas y arreglo de bugs

--- content-script.js
+++ content-script.js
@@ -1,3 +1,94 @@
+let nextchap = ""
+let nextchaplink = ""
+let previuschap = ""
+let prevchaplink = ""
+
+const getNextChapter = () => {
+  nextchap = document.getElementsByClassName(
+    "col-6 col-sm-2 order-2 order-sm-3 chapter-arrow chapter-next"
+  )[0]
+
+  try {
+    nextchaplink = nextchap.getElementsByTagName("a")[0].href
+  } catch (error) {
+    return
+  }
+}
+
+getNextChapter()
+
+const getPrevChapter = () => {
+  previuschap = document.getElementsByClassName(
+    "col-6 col-sm-2 order-1 order-sm-1 chapter-arrow chapter-prev"
+  )[0]
+
+  try {
+    prevchaplink = previuschap.getElementsByTagName("a")[0].href
+  } catch (error) {
+    return
+  }
+}
+
+getPrevChapter()
+
+nextchap.addEventListener("click", (e) => {
+  e.preventDefault()
+
+  chrome.storage.sync.get("noRedirect", ({ noRedirect }) => {
+    if (noRedirect) {
+      fetch(nextchaplink)
+        .then((res) => res.text())
+        .then((data) => {
+          const parser = new DOMParser()
+          const doc = parser.parseFromString(data, "text/html")
+          const stringurl = doc.getElementsByClassName(
+            "btn btn-xs btn-social btn-primary"
+          )[0].attributes.onclick.value
+          chrome.storage.sync.get("cascadeMode", ({ cascadeMode }) => {
+            if (cascadeMode) {
+              const cascade = stringurl.replace("paginated", "cascade")
+              location.href = cascade.match(/'([^']+)'/)[1]
+            }
+          })
+          location.href = stringurl.match(/'([^']+)'/)[1]
+        })
+      return
+    }
+    location.href = nextchaplink
+  })
+})
+
+previuschap.addEventListener("click", (e) => {
+  e.preventDefault()
+
+  chrome.storage.sync.get("noRedirect", ({ noRedirect }) => {
+    try {
+      if (noRedirect) {
+        fetch(prevchaplink)
+          .then((res) => res.text())
+          .then((data) => {
+            const parser = new DOMParser()
+            const doc = parser.parseFromString(data, "text/html")
+            const stringurl = doc.getElementsByClassName(
+              "btn btn-xs btn-social btn-primary"
+            )[0].attributes.onclick.value
+            chrome.storage.sync.get("cascadeMode", ({ cascadeMode }) => {
+              if (cascadeMode) {
+                const cascade = stringurl.replace("paginated", "cascade")
+                location.href = cascade.match(/'([^']+)'/)[1]
+              }
+            })
+            location.href = stringurl.match(/'([^']+)'/)[1]
+          })
+        return
+      }
+      location.href = prevchaplink
+    } catch (error) {
+      return
+    }
+  })
+})
+
 chrome.storage.sync.get("cascadeMode", ({ cascadeMode }) => {
   if (cascadeMode) {
     const url = document.URL
@@ -20,16 +111,79 @@ chrome.storage.sync.get("hidePointer", ({ hidePointer }) => {
 })
 
 chrome.storage.sync.get("changeChapter", ({ changeChapter }) => {
-  if (changeChapter) {
-    document.onkeydown = checkKey
+  try {
+    if (changeChapter) {
+      document.onkeydown = checkKey
 
-    function checkKey(e) {
-      e = e || window.event
+      function checkKey(e) {
+        e = e || window.event
 
-      if (e.keyCode == "37")
-        document.getElementsByClassName("fas fa-backward fa-2x")[0].click()
-      if (e.keyCode == "39")
-        document.getElementsByClassName("fas fa-forward fa-2x")[0].click()
+        chrome.storage.sync.get("noRedirect", ({ noRedirect }) => {
+          if (noRedirect) {
+            if (e.keyCode == "37") {
+              e.preventDefault()
+              const previuschapter = document.getElementsByClassName(
+                "col-6 col-sm-2 order-1 order-sm-1 chapter-arrow chapter-prev"
+              )[0]
+              const linkprevchapter =
+                previuschapter.getElementsByTagName("a")[0].href
+
+              fetch(linkprevchapter)
+                .then((res) => res.text())
+                .then((data) => {
+                  const parser = new DOMParser()
+                  const doc = parser.parseFromString(data, "text/html")
+                  const stringurl = doc.getElementsByClassName(
+                    "btn btn-xs btn-social btn-primary"
+                  )[0].attributes.onclick.value
+                  chrome.storage.sync.get("cascadeMode", ({ cascadeMode }) => {
+                    if (cascadeMode) {
+                      const cascade = stringurl.replace("paginated", "cascade")
+                      location.href = cascade.match(/'([^']+)'/)[1]
+                    }
+                  })
+                  location.href = stringurl.match(/'([^']+)'/)[1]
+                })
+              return
+            }
+
+            if (e.keyCode == "39") {
+              e.preventDefault()
+              const nextchapter = document.getElementsByClassName(
+                "col-6 col-sm-2 order-2 order-sm-3 chapter-arrow chapter-next"
+              )[0]
+              const linknextchapter =
+                nextchapter.getElementsByTagName("a")[0].href
+
+              fetch(linknextchapter)
+                .then((res) => res.text())
+                .then((data) => {
+                  const parser = new DOMParser()
+                  const doc = parser.parseFromString(data, "text/html")
+                  const stringurl = doc.getElementsByClassName(
+                    "btn btn-xs btn-social btn-primary"
+                  )[0].attributes.onclick.value
+                  chrome.storage.sync.get("cascadeMode", ({ cascadeMode }) => {
+                    if (cascadeMode) {
+                      const cascade = stringurl.replace("paginated", "cascade")
+                      location.href = cascade.match(/'([^']+)'/)[1]
+                    }
+                  })
+                  location.href = stringurl.match(/'([^']+)'/)[1]
+                })
+              return
+            }
+          }
+
+          if (e.keyCode == "37")
+            document.getElementsByClassName("fas fa-backward fa-2x")[0].click()
+
+          if (e.keyCode == "39")
+            document.getElementsByClassName("fas fa-forward fa-2x")[0].click()
+        })
+      }
     }
+  } catch (error) {
+    return
   }
 })
--- library.js
+++ library.js
@@ -1,3 +1,62 @@
+let newChapters = document.querySelectorAll('[class="chapter-viewed-icon"]')
+let viewedChapters = document.querySelectorAll(
+  '[class="chapter-viewed-icon viewed"]'
+)
+
+let newChapter = newChapters[newChapters.length - 1]
+let lastviewed = viewedChapters[0]
+
+const newButton = () => {
+  if (window.location.href.indexOf("orderDir=ASC") > -1) {
+    return
+  }
+
+  if (lastviewed) {
+    document.getElementsByClassName(
+      "col-12 col-md-9 element-header-content-text"
+    )[0].innerHTML += `<h4 class="element-title my-2">Último capítulo leído: ${
+      lastviewed.parentElement.parentElement.getElementsByTagName("div")[0]
+        .innerText
+    }</h4>`
+  }
+
+  if (!lastviewed) {
+    document.getElementsByClassName(
+      "col-12 col-md-9 element-header-content-text"
+    )[0].innerHTML += `<button id="golastchapter" class="badge-primary py-2 px-4 mx-1 my-2 btn">Empezar a leer</button>`
+  } else {
+    try {
+      document.getElementsByClassName(
+        "col-12 col-md-9 element-header-content-text"
+      )[0].innerHTML += `<button id="golastchapter" class="badge-primary py-2 px-4 mx-1 my-2 btn">Continuar con el ${
+        newChapter.parentElement.parentElement.getElementsByTagName("div")[0]
+          .innerText
+      }</button>`
+    } catch (error) {
+      return
+    }
+  }
+
+  newChapter.parentElement.parentElement
+    .getElementsByTagName("div")[0]
+    .setAttribute("tabindex", "-1")
+
+  try {
+    document.getElementById("show-chapters").style.display = "none"
+    document.getElementById("chapters-collapsed").style.display = "block"
+  } catch (error) {
+    return
+  }
+}
+
+newButton()
+
+let golastchapter = document.getElementById("golastchapter")
+
+golastchapter?.addEventListener("click", () => {
+  newChapter.parentElement.parentElement.getElementsByTagName("div")[0].focus()
+})
+
 chrome.storage.sync.get("noRedirect", ({ noRedirect }) => {
   if (noRedirect) {
     let chapters = document.getElementsByClassName("btn btn-default btn-sm")
