410e652e8a5d5cc717f6c817bd0078794b359047
Tou-u <rodrignacio99@hotmail.com>
2022-08-23 03:34:34 -0400

    Desactiva la extensión si no estás en tmo

--- background.js
+++ background.js
@@ -4,7 +4,23 @@ let hidePointer = false
 let noRedirect = true
 let changeChapter = true
 
+chrome.action.disable()
+
 chrome.runtime.onInstalled.addListener(() => {
+  chrome.declarativeContent.onPageChanged.removeRules(() => {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: chrome.runtime.getManifest().host_permissions.map((h) => {
+          const [, sub, host] = h.match(/:\/\/(\*\.)?([^/]+)/)
+          return new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: sub ? { hostSuffix: "." + host } : { hostEquals: host },
+          })
+        }),
+        actions: [new chrome.declarativeContent.ShowAction()],
+      },
+    ])
+  })
+
   chrome.storage.sync.set({ cascadeMode })
   chrome.storage.sync.set({ darkMode })
   chrome.storage.sync.set({ hidePointer })
